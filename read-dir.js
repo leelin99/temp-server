76f8e7267762423e03d61d52c38c7612fb7a28f1
lee <759757929@qq.com>
2023-03-15 23:07:12 +0800

    修复

--- read-dir.js
+++ read-dir.js
@@ -1,30 +1,30 @@
-// const fs = require('fs')
-// const { resolve } = require('path')
-// const nameArr = fs.readdirSync(resolve("./static/image"),{encode: "utf-8", writeFileTypes: true})
-// const collectObj ={}
-// const noExtNameArr = nameArr.map(el => el.split('.')[0])
-// for(let name of noExtNameArr) {
-//   const originName = name.split(' ')[0]
-//   if(!collectObj[originName]) {
-//     collectObj[originName] = 1
-//   }
-// }
-// typeArr = {}
-// Object.keys(collectObj).forEach(key => {
-//   for(let name of nameArr) {
-//     if(name.includes(key) ){
-//       if(!typeArr[key]) {
-//         typeArr[key] = [name];
-//         continue
-//       }   
-//       typeArr[key].push(name)
-//     }
-//   }
-// });
-// const data = Object.values(typeArr).map((el, index) => ({id: index, value: el}))
-// console.log(data)
-// fs.writeFile('./image-list.json', JSON.stringify(data), (err, data) => {
-//   console.log(err, "err")
-// })
-const imageList = require("./image-list.json")
-console.log(imageList)
+const fs = require('fs')
+const { resolve } = require('path')
+const nameArr = fs.readdirSync(resolve("./static/image"),{encode: "utf-8", writeFileTypes: true})
+const collectObj ={}
+const noExtNameArr = nameArr.map(el => el.split('.')[0])
+for(let name of noExtNameArr) {
+  const originName = name.split(' ')[0]
+  if(!collectObj[originName]) {
+    collectObj[originName] = 1
+  }
+}
+typeArr = {}
+Object.keys(collectObj).forEach(key => {
+  for(let name of nameArr) {
+    if(name.includes(key) ){
+      if(!typeArr[key]) {
+        typeArr[key] = [name];
+        continue
+      }   
+      typeArr[key].push(name)
+    }
+  }
+});
+const data = Object.values(typeArr).map((el, index) => ({id: index, value: el}))
+console.log(data)
+fs.writeFile('./image-list.json', JSON.stringify(data), (err, data) => {
+  console.log(err, "err")
+})
+// const imageList = require("./image-list.json")
+// console.log(imageList)
